9333e43173e0739f981f1b379cdda27057da0af5
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-19 12:55:37 -0400

    Add ability to get a single book by specific author

--- src/Library.API/Controllers/BooksController.cs
+++ src/Library.API/Controllers/BooksController.cs
@@ -31,5 +31,24 @@ namespace Library.API.Controllers
 
             return Ok(booksForAuthor);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookForAuthor(Guid authorId, Guid id)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if (bookForAuthorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            var bookForAuthor = Mapper.Map<BookDto>(bookForAuthorFromRepo);
+
+            return Ok(bookForAuthor);
+        }
     }
 }
