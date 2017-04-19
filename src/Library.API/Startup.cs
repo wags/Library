934bc40d4efafede29a29f93956c63902885e7a1
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-19 12:45:50 -0400

    Update AutoMapper config for Book mapping
    No custom projections needed on this one.

--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -71,6 +71,8 @@ namespace Library.API
                     $"{src.FirstName} {src.LastName}"))
                     .ForMember(dest => dest.Age, opt => opt.MapFrom(src =>
                     src.DateOfBirth.GetCurrentAge()));
+
+                cfg.CreateMap<Entities.Book, Models.BookDto>();
             });
 
             libraryContext.EnsureSeedDataForContext();
