489ce49419daf3abb5b556e2ed75650f3e79e5ab
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-19 09:42:00 -0400

    Implement outer-facing contract
    Add an Authors Controller.

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,21 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
