2cf692d537a8a3a949fbb6bdab93de35a1391af9
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-19 12:45:17 -0400

    Add books controller

---
+++ src/Library.API/Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors/{authorId}/books")]
+    public class BooksController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public BooksController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetBooksForAuthor(Guid authorId)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var booksForAuthorFromRepo = _libraryRepository.GetBooksForAuthor(authorId);
+
+            var booksForAuthor = Mapper.Map<IEnumerable<BookDto>>(booksForAuthorFromRepo);
+
+            return Ok(booksForAuthor);
+        }
+    }
+}
