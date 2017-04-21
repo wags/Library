8559719e51831c7bb970ed5681feb6dddc9a3edb
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-21 09:10:18 -0400

    Add an action for deleting a book resource

--- src/Library.API/Controllers/BooksController.cs
+++ src/Library.API/Controllers/BooksController.cs
@@ -81,5 +81,29 @@ namespace Library.API.Controllers
                 new { authorId = authorId, id = bookToReturn.Id },
                 bookToReturn);
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBookForAuthor(Guid authorId, Guid id)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if (bookForAuthorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _libraryRepository.DeleteBook(bookForAuthorFromRepo);
+
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception($"Deleting book {id} for author {authorId} failed on save.");
+            }
+
+            return NoContent();
+        }
     }
 }
