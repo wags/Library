efd4113fa39f109563ebb5dd07778ce1bf318d2e
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-26 12:37:54 -0400

    Customize error messages in book DTO

--- src/Library.API/Models/BookForCreationDto.cs
+++ src/Library.API/Models/BookForCreationDto.cs
@@ -4,11 +4,11 @@ namespace Library.API.Models
 {
     public class BookForCreationDto
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "You should include a title.")]
+        [MaxLength(100, ErrorMessage = "The title may not contain more than 100 characters.")]
         public string Title { get; set; }
 
-        [MaxLength(500)]
+        [MaxLength(500, ErrorMessage = "The description may not have more than 500 characters.")]
         public string Description { get; set; }
     }
 }
