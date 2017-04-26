70d0618f92c3d37203fad7d938bea76cb3620a70
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-26 13:08:18 -0400

    Add book validation on PUT request
    Similar to POST, but note that BookForUpdateDto is used instead of
    BookForCreateDto.

--- src/Library.API/Controllers/BooksController.cs
+++ src/Library.API/Controllers/BooksController.cs
@@ -129,6 +129,17 @@ namespace Library.API.Controllers
                 return BadRequest();
             }
 
+            if (book.Description == book.Title)
+            {
+                ModelState.AddModelError(nameof(BookForUpdateDto),
+                    "The provided description should be different from the title.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return new UnprocessableEntityObjectResult(ModelState);
+            }
+
             if (!_libraryRepository.AuthorExists(authorId))
             {
                 return NotFound();
