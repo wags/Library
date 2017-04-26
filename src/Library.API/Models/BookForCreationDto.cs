4fbc2c7ab3a6e80292d38d58e9ad14ef4f66dd4e
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-26 12:58:01 -0400

    Add an abstract class for book modification
    Keep code DRY.

--- src/Library.API/Models/BookForCreationDto.cs
+++ src/Library.API/Models/BookForCreationDto.cs
@@ -1,14 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Library.API.Models
+﻿namespace Library.API.Models
 {
-    public class BookForCreationDto
+    public class BookForCreationDto : BookForManipulationDto
     {
-        [Required(ErrorMessage = "You should include a title.")]
-        [MaxLength(100, ErrorMessage = "The title may not contain more than 100 characters.")]
-        public string Title { get; set; }
 
-        [MaxLength(500, ErrorMessage = "The description may not have more than 500 characters.")]
-        public string Description { get; set; }
     }
 }
---
+++ src/Library.API/Models/BookForManipulationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.API.Models
+{
+    public abstract class BookForManipulationDto
+    {
+        [Required(ErrorMessage = "You should include a title.")]
+        [MaxLength(100, ErrorMessage = "The title may not contain more than 100 characters.")]
+        public string Title { get; set; }
+
+        [MaxLength(500, ErrorMessage = "The description may not have more than 500 characters.")]
+        public string Description { get; set; }
+    }
+}
