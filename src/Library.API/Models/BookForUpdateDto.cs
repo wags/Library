4d8c1745db1d09561a9a5d9336db7eec7dd2257b
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-26 13:02:31 -0400

    Require a description when updating a book

--- src/Library.API/Models/BookForUpdateDto.cs
+++ src/Library.API/Models/BookForUpdateDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Library.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.API.Models
 {
     public class BookForUpdateDto : BookForManipulationDto
     {
-
+        [Required(ErrorMessage = "You should include a description.")]
+        public override string Description { get => base.Description; set => base.Description = value; }
     }
 }
