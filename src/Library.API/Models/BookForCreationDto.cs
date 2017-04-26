c81c343ea00bd9e69b545a3749235b386fb63fd5
Matt Wagner <m.wagner@dwdiesel.com>
2017-04-26 12:25:36 -0400

    Add data annotations for book validation

--- src/Library.API/Models/BookForCreationDto.cs
+++ src/Library.API/Models/BookForCreationDto.cs
@@ -1,8 +1,14 @@
-﻿namespace Library.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.API.Models
 {
     public class BookForCreationDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [MaxLength(500)]
         public string Description { get; set; }
     }
 }
