f5f953059c5827629c10442ac3f3c445b6e097cf
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-01 12:06:38 -0400

    Update query to support paging in implementation
    Use AuthorsResourceParameters to support paging in the implementation by
    adding Skip and Take to the query.

--- src/Library.API/Services/LibraryRepository.cs
+++ src/Library.API/Services/LibraryRepository.cs
@@ -1,7 +1,8 @@
-﻿using Library.API.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Library.API.Entities;
+using Library.API.Helpers;
 
 namespace Library.API.Services
 {
@@ -64,9 +65,15 @@ namespace Library.API.Services
             return _context.Authors.FirstOrDefault(a => a.Id == authorId);
         }
 
-        public IEnumerable<Author> GetAuthors()
+        public IEnumerable<Author> GetAuthors(
+            AuthorsResourceParameters authorsResourceParameters)
         {
-            return _context.Authors.OrderBy(a => a.FirstName).ThenBy(a => a.LastName);
+            return _context.Authors
+                .OrderBy(a => a.FirstName)
+                .ThenBy(a => a.LastName)
+                .Skip(authorsResourceParameters.PageSize * (authorsResourceParameters.PageNumber - 1))
+                .Take(authorsResourceParameters.PageSize)
+                .ToList();
         }
 
         public IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds)
