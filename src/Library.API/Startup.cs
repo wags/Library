333c82beb0d58a4f579a00b137972024e1436842
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-05 12:55:13 -0400

    Add contract resolver to camel-case property names
    After shaping the data, when serialized, the properties were not being
    presented in camel-case anymore. Adding this JSON configuration option
    resolves that issue.

--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using NLog.Extensions.Logging;
 using NLog.Web;
 
@@ -44,6 +45,11 @@ namespace Library.API
                 setupAction.ReturnHttpNotAcceptable = true;
                 setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
                 setupAction.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
+            })
+            .AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
             });
 
             // register the DbContext on the container, getting the connection string from
